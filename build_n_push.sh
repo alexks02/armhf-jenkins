4bf35534a0d584dca9b7299af5ba585ac278e718
alexks02 <alex.ks02@gmail.com>
2019-07-16 12:08:21 -0400

    Jenkins version updated

--- build_n_push.sh
+++ build_n_push.sh
@@ -3,4 +3,4 @@
 sudo docker-compose build
 sudo docker-compose push
 sudo docker-compose rm -f
-sudo docker images -q | xargs docker rmi 2>/dev/null
+sudo docker images -q | xargs sudo docker rmi 2>/dev/null
